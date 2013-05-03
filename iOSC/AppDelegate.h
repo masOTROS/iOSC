f4a4fd1950954e20f8fe78fdd4c2d678b4f72147
masOTROS <javier@masotros.com>
2013-05-03 18:26:02 -0300

    Initial Commit

---
+++ iOSC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSC
+//
+//  Created by Wanda on 03/05/13.
+//  Copyright (c) 2013 +OTROS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSC
+//
+//  Created by Wanda on 03/05/13.
+//  Copyright (c) 2013 +OTROS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
